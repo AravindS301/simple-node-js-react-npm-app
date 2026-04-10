10bf39b5fd2284ba163eccf86eddd4166d80e77b
Aravind <aravindeightten2002@gmail.com@gmail.com>
2026-04-10 10:48:57 +0000

    Fixed kill.sh to avoid failure if process not found

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,4 +4,13 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
-kill $(cat .pidfile)
+if [ -f .pidfile ]; then
+  PID=$(cat .pidfile)
+
+  # Check if process exists before killing
+  if ps -p $PID > /dev/null 2>&1; then
+    kill $PID
+  else
+    echo "Process already stopped"
+  fi
+fi
